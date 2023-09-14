2243ea85a876f4bbbf2bc17b6d31e53608274aab
Pacific077 <2021ugcs077@nitjsr.ac.in>
2023-09-14 11:03:27 +0530

    created signup page frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandinPage from './components/landingpage/LandinPage';
 import Login from './components/login/Login';
+import Signup from './components/signup/Signup';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route  path='/' element = {<LandinPage/>} />
       <Route  path='/login' element = {<Login/>} />
+      <Route  path='/signup' element = {<Signup/>} />
     </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/components/signup/Signup.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./signup.css";
+const Signup = () => {
+  return (
+    <div className="signuppage">
+      <div className="sihnfrsthalf">
+        <h1>Virtual Manager</h1>
+        <div className="smalldesc">
+          Welcome to your virtual manager page. Navigate tasks, monitor
+          progress, and optimize your operations effectively..
+        </div>
+        <div className="productsNumber">1 M+</div>
+        <div className="productnumberdesc">More than 100,000 customers</div>
+        <div className="buyersnumbers">100,000+</div>
+        <div className="sbuyersnumberdesc">More than 100,000 customers</div>
+        <div className="sellernumbers">4000+</div>
+        <div className="sellernumberdesc">More than 4000 sellers</div>
+      </div>
+      <div className="sign2ndhalf">
+        <h1>Join Now</h1>
+        <form className="signupform" action="post">
+          <label htmlFor="">Email</label>
+          <input type="email" placeholder="abc@examole.com" />
+          <label htmlFor="">Name</label>
+          <input type="text" placeholder="Name" />
+          <label htmlFor="">Password</label>
+          <input type="password" placeholder="Password" />
+          <button className="signupbtn" type="submit">
+            Signup
+          </button>
+          <p className="alreadyaccount">
+            Already have an account ?&nbsp;&nbsp;&nbsp;{" "}
+            <Link className="link" to="/Login">Login</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
