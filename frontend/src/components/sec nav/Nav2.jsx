8b9e81fa3559dd21e09a75e3e61342253ffd0cd9
Pacific077 <2021ugcs077@nitjsr.ac.in>
2023-09-23 21:48:53 +0530

    completed nav2 designs

--- frontend/src/components/sec nav/Nav2.jsx
+++ frontend/src/components/sec nav/Nav2.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRightFromBracket, faBell, faGear, faMessage, } from '@fortawesome/free-solid-svg-icons'
+import "./nav2.css"
 const Nav2 = () => {
   return (
     <div className="homenav2">
-    nav2
-</div>
+      <h3 className='nav2head'>Virtual Manager</h3>
+    <div className="nav2iconscont">
+    <FontAwesomeIcon className='navtwoicons' icon={faBell} size='xl' color='white' />
+    <FontAwesomeIcon className='navtwoicons' icon={faGear} size='xl' color='white' />
+    <FontAwesomeIcon className='navtwoicons' icon={faMessage} size='xl' color='white' />
+    <FontAwesomeIcon className='navtwoicons' icon={faArrowRightFromBracket} size='xl' color='white' />
+    </div>
+    </div>
   )
 }
 
